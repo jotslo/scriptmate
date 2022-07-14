96b42c13ea820d2193f106fa18a228c57520c978
Josh <me@joshl.io>
2022-07-15 00:34:51 +0100

    implemented fix for modulescript behaviour

--- api/test.lua
+++ api/test.lua
@@ -1,26 +1,4 @@
-local name = 'NewPart' .. tostring(math.random(1000, 9999)) local part = Instance.new('Part') part.Name = name part.Parent = workspace wait() local log = game:GetService('LogService'):GetLogHistory() if log[#log-i].message:find(name) then return true end return false
-
---[[wait()
-local log = game:GetService('LogService'):GetLogHistory()
-
-local len = 0
-for i, v in workspace:GetChildren() do
-    len += 1
+if userCode:find('require') ~= nil and not userCode:find('print(\"') and not userCode:find('print(\'') then
+    return 'local newRequire = require local function require(moduleScript) return newRequire:Clone()' .. userCode
 end
-
-local function find(msg)
-    for i, v in workspace:GetChildren() do
-        if msg:find(v.Name) then
-            return true
-        end
-    end
-    return false
-end
-
-for i = 0, len - 1 do
-    if not find(log[#log-i].message) then
-        return false
-    end
-end
-
-return true]]
+return false
