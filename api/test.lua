b9fc9451c2a688a51e2430fcef78999b97c43866
Josh <me@joshl.io>
2022-07-14 16:20:03 +0100

    implemented episode 6

--- api/test.lua
+++ api/test.lua
@@ -1,14 +1,16 @@
-wait()
+local name = 'NewPart' .. tostring(math.random(1000, 9999)) local part = Instance.new('Part') part.Name = name part.Parent = workspace wait() local log = game:GetService('LogService'):GetLogHistory() if log[#log-i].message:find(name) then return true end return false
+
+--[[wait()
 local log = game:GetService('LogService'):GetLogHistory()
 
 local len = 0
-for i, v in myTable do
+for i, v in workspace:GetChildren() do
     len += 1
 end
 
 local function find(msg)
-    for i, v in myTable do
-        if msg:find(i) and msg:find(v) then
+    for i, v in workspace:GetChildren() do
+        if msg:find(v.Name) then
             return true
         end
     end
@@ -21,4 +23,4 @@ for i = 0, len - 1 do
     end
 end
 
-return true
+return true]]
