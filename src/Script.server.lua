039f64536c6ab84f34b783081fa6d5a33d17049f
Josh <me@joshl.io>
2022-12-08 04:44:26 +0000

    Remove unnecessary file

--- src/Script.server.lua
+++
@@ -1,4 +0,0 @@
--- Before completing this exercise, please watch the episode below.
--- Have feedback? Feel free to send me a tweet @ twitter.com/jotslo
--- EPISODE 1: https://youtube.com/watch?v=abcdefghij
-
