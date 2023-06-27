31b0f1c8d3e7f5def842d19cfb031218faed0869
Josh <me@joshl.io>
2023-06-27 21:16:58 +0100

    fix broken data & add most solutions

---
+++ solutions/05_functions/01_basic_function.lua
@@ -0,0 +1,8 @@
+-- declare a function named myFunction
+-- when called, output number 15
+function myFunction()
+    print(15)
+end
+
+-- call the function
+myFunction()
---
+++ solutions/08_remotes/01_remote_events.lua
@@ -0,0 +1,1 @@
+-- we assume we're
